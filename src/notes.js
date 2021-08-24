d66536d86b7e1b6f570ea42348add796dad10bcd
Francisco-Lemos <fr.lemos.12@gmail.com>
2021-08-24 20:27:31 -0300

    Aprimoramento do módulo notes com código mais elegante

--- src/notes.js
+++ src/notes.js
@@ -12,27 +12,24 @@ function message_people_not_find(person_name){
     return chalk.red.bgWhite(`Error message: A pessoa de nome ${person_name} não consta no banco de dados!`);
 }
 
-function index_for_people(dict, person_name){
-    for (var i = 0; i < dict.length; i++){
-        if (dict[i].nome == person_name) return i;
-    }
-    return null;
+function return_index(person_name){
+    lista = list();
+    index = lista.findIndex( item => item.nome == person_name);
+    return index;
 }
 
 function add(dados){
-    dict = list();
-    index = index_for_people(dict, dados.nome);
-    if (index != null){
-        return chalk.red(`Error: A pessoa de nome ${dados.nome} já está cadastrada em nosso banco de dados!`) +chalk.white("\nPara alteração de dados use o comando edit. Para remoção use o comando remove_person.");
+    index = return_index(dados.nome);
+    if (index == -1){
+        dict.push(dados);
+        return write_in_file(dict);
     }
-    dict.push(dados);
-    return write_in_file(dict);
+    return chalk.red(`Error: A pessoa de nome ${dados.nome} já está cadastrada em nosso banco de dados!`) +chalk.white("\nPara alteração de dados use o comando edit. Para remoção use o comando remove_person.");
 }
 
 function edit(dados){
-    dict = list();
-    index = index_for_people(dict, dados.nome);
-    if (index != null){
+    index = return_index(dados.nome);
+    if (index > -1){
         dict[index] = Object.assign(dict[index], dados);
         return write_in_file(dict);
     }
@@ -40,9 +37,8 @@ function edit(dados){
 }
 
 function remove_person(person_name){
-    dict = list();
-    index = index_for_people(dict, person_name);
-    if (index != null){
+    index = return_index(dados.nome);
+    if (index > -1){
         dict.splice(index, 1);
         return write_in_file(dict);
     }
@@ -50,9 +46,8 @@ function remove_person(person_name){
 }
 
 function remove_attr(person_name, attr_person){
-    dict = list();
-    index = index_for_people(dict, person_name);
-    if (index != null){
+    index = return_index(dados.nome);
+    if (index > -1){
         if (dict[index][attr_person]){
             delete dict[index][attr_person]
             return write_in_file(dict);
